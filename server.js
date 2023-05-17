ccc8b37db72028aeb0e4eb0e87fb5c9f1acc0b75
Junior David Roche <100164051+DJRoche509@users.noreply.github.com>
2023-05-16 23:21:23 -0500

    Comment out defined out routes

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ app.use(express.json());
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname,'./public/index.html'))
 })
-app.get('/styles', (req,res) => {
-    res.sendFile(path.join(__dirname,'./public/styles.css'))
-})
-app.get('/index', (req,res) => {
-    res.sendFile(path.join(__dirname,'./public/index.js'))
-})
+// app.get('/styles', (req,res) => {
+//     res.sendFile(path.join(__dirname,'./public/styles.css'))
+// })
+// app.get('/index', (req,res) => {
+//     res.sendFile(path.join(__dirname,'./public/index.js'))
+// })
 
 
 // Add up the total health of all the robots
